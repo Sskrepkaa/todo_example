4d9339293adf7e331e29e8aae5168a8c33c8ae02
sskrepkaa <makuhinmaks@ya.ru>
2023-04-07 17:42:59 +0300

    init

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://olacwjwwvuaimvgmezgz.supabase.co', '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sYWN3and3dnVhaW12Z21lemd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA3MTcwNTgsImV4cCI6MTk5NjI5MzA1OH0.2C2e_V_ypqqsYUDB_IoRtojo4VqsTWZnKyCEE5BqECw>')
